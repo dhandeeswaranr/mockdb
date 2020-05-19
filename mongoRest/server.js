1e3a091de9c771120371be09a2b782e50fc40e10
dhandeeswaran <dhandeeswaran.r@gmail.com>
2020-05-19 16:36:53 +0530

    commited to master

---
+++ mongoRest/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// create express app
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }, {useUnifiedTopology: true}))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.header("Access-control-Allow-Origin", "*");
+    res.header("Access-control-Allow-Headers", "Origin, x-Requested-With, Content-Type, Accept");
+    next();
+
+} )
+
+
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+
+require('./app/routes/note.routes.js')(app);
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
