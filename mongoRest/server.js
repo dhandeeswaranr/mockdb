b3825166c6067d97439d9ce4561ace2bbf3dfa96
dhandeeswaran <dhandeeswaran.r@gmail.com>
2020-05-22 16:25:16 +0530

    delete updated

--- mongoRest/server.js
+++ mongoRest/server.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json())
 // allow orgin
 app.use((req, res, next) => {
     res.header("Access-control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     res.header("Access-control-Allow-Headers", "Origin, x-Requested-With, Content-Type, Accept");
     next();
 
